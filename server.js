205680558264c30c3707ec91fc0da0a692d67392
unknown <safavimoghadam.89@gmail.com>
2020-08-02 01:06:52 +0430

    modify port

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const logger = require('morgan');
 var cors = require('cors');
 app.use(cors());
 var items = require('./descriptor.json');
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
+app.set('port', process.env.PORT || 5000);
 
 /* app.use */
 app.use(logger('dev'));
@@ -61,5 +62,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+app.listen(app.get('port'));
 
-app.listen(PORT);
+//app.listen(PORT);
