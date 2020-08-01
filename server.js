e51b3b46f7f1066452a9325b2d1a32c060b7e2b1
unknown <safavimoghadam.89@gmail.com>
2020-08-02 00:51:55 +0430

    modify

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const dotenv = require("dotenv");
 dotenv.config();
+const path = require('path');
 const logger = require('morgan');
 var cors = require('cors');
 app.use(cors());
@@ -48,5 +49,17 @@ app.post('/getData', function(req, res) {
     }
 
 });
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 app.listen(PORT);
